4bd7b946b7f6388d9de02ab2c6de5380e23b5888
FiveFourTEC <52660189+joelhoffmann@users.noreply.github.com>
2021-06-27 15:01:21 +0200

    first

---
+++ src/Kursbericht.java
@@ -0,0 +1,91 @@
+import cfg.schueler;
+import cfg.lehrer;
+import cfg.schulhaus;
+import welt.virus.COV19;
+import getraenk.kaffee;
+import getraenk.clubmate;
+
+public class Kursbericht {
+
+    public static void main(String[] args) {
+        /* no comment */
+
+        schueler hackerman = new schueler();
+        lehrer straeten = new lehrer("Straeten");
+
+        while (hackerman.ABITUR_cannot_be_found()) {
+            if (hackerman.class == 10) {
+                /*
+                Um in das Pilotprojekt "Informatik für Dullis" am CFG zu kommen musste man ähnlich zum KSK oder der GSG9 ein hartes Einstellungsverfahren absolvieren.
+                Es bestand aus 25 Kilobyte Fitness-Surf-Test und einem Hardwarecheck auf Prozessor und Platine. Da dieser einfach viel zu simpel war, hatten wir auch Teilnehmer,
+                welche es wiederholt geschafft haben, den USB-Stick in den Netzwerk-Port zu stecken. Dennoch bestand diese Arbeitsgemeinschaft aus +- 20 wissbegierigen Schülern.
+                Abschließend sollte eine gemeinsame Exkursion zu einem Workshop bei SAP stattfinden. Dieser startete im allseits bekanntem Inspirationpavilion und endete in einem Raum voller Computer und Hackermans.
+                Diese bekamen die Aufgabe, sich Wissen zum Thema HTML/CSS anzueignen. Da einigen hochleistungs-Hackermans diese Aufgabe etwas zu langweilig war. Aufgrund zu viel Vorerfahrung, endete es in n + 1 offenen Tabs
+                (hierbei ist n nicht zählbar und die Anzahl der offenen Tabs *hust* Rekursion ohne Abbruchbedingung *hust* (Um Rekursion zu verstehen, muss man zunächst Rekursion verstehen!)).
+                 */
+                hackerman.class++;
+            }
+            if (hackerman.class == 11) {
+                /*
+                Aufgrund Budgetkürzungen für die Schüler der Arbeitsgemeinschaft, gab es nur Platz für 11 Hackermans und 1 Lehrer. CFG ist halt pleite (siehe nicht vorhandene Decke).
+                Diese 11 Hackermans teilten sich in 8 unerfahrene Leistungskursler und 3 unfähige Basiskursler auf. Nur der Lehrkörper war einigermaßen kompetent solang es nicht um das WLAN ging.
+                Apropos WLAN: (Zensur, Frau Straeten möchte keinen weiteren Satz über das WLAN in diesem Kursbericht sehen).
+
+                Also zurück in die digitale Welt. Neben Theoriethemen wie: (das würde den Rahmen sprengen), haben wir auch praktisch programmiert. Mehr oder weniger erfolgreich und auch mehr oder weniger selbstständig.
+                Hierbei möchten wird kurz den Bundeswettbewerb Informatik zur Sprache bringen. In der ersten Runde waren die Aufgaben für unsere, noch nicht ausgereifte Prozessortechnik, viel zu schwer,
+                sodass Verzweiflung und häufiges Fluchen ("ICH HASSE BIEBER!!11elf") keine Seltenheit war. Wir konnten ja nich wissen, dass ein Leben als Informatiker so Hardware.
+                Allerdings gab es immer wieder kleine Hoffnungsfunken, wenn man sich in den Tiefen des Internets (google-Suche Seite 2, auf der die ganzen Leichen versteckt werden) bewegte.
+                Dank unseres Fitness-Surf-Test und weiteren Trainingseinheiten waren wir in der Lage auch mal über den Rand der ersten Google-Suchseite hinaus zu blicken.
+                An dieser Stelle, danke an das Internet für zahlreichen Informatikseiten und der Copy-Paste-Funktion *hust* stackoverflow *hust*.
+                Doch die Copy-Paste Funktion ließ uns auch immer wieder etwas Lernen, und der Unterricht von Frau Straeten natürlich auch, was auch zu etwas erfolgreicheren Projekten führte.
+                Wir schafften es sogut wie jeden bekannten Sortieralgorithmus in unserer Lieblingssprache Java und der noch viel beliebteren Umgebung BlueJ zu implementieren.
+                Aufgrund dieser enormen Lernbegeisterung waren wir auch in weiteren Informatikwettbewerben vertreten, wie z.B. (alles Wettbewerbe die eh keiner googelt (gibts das Wort googelt?)).
+                Diese Einzelwettbewerbe wurden wegen der guten Breitbandanbindung zwischen den Hackermans im Team DATENBACH meist eher zusammen gelöst. Ein Hackerman war besonders schnell bei diesen Wettbewerben.
+                Er war nach 20 von 45 Minuten fertig. Dieser teilte Brüderlich/Schwesterlich seine Lösungen. Aber das weiß ja eh niemand bis auf unser FBI-Agent, der jedesmal weinend auf seinem Stuhl saß.
+                Das tat manchmal auch Frau Straeten wenn sie unsere, auf Papier geschriebenen, Implementierungen in den Klausuren korrigieren musste.
+                */
+                COV19.activate();
+                /*
+                Noch dazu sollte das Leben der 11 Hackermans und das von Frau Straeten durch einen sehr brutalen Virus beeinträchtigt werden. Er war in der Lage, sämtliche Schutzsysteme zu umgehen und machte es unmöglich,
+                dass sich die Arbeitsgemeinschaft der Hackermans ihrem Gemeinschaftskeller treffen konnte. Doch die digitalen Fähigkeiten machten es den Hackermans im Vergleich zu dem Rest der Schule deutlich einfacher,
+                die Arbeitsgeschmeinschaftstreffen online stattfinden zu lassen um so den Lernprozess nicht zu schädigen. Allerdings war diese Form des Unterrichts für alle meist sehr viel anstrengender als vorher.
+                Aber trotz aller Soft-und Hardwarebelastung überlebten die 11 Hackermans und die eine dazugehörige Lehrerin das Schuljahr.
+                 */
+                hackerman.class++;
+            }
+            if (hackerman.class == 12) {
+                /*
+
+                Die Sommerferien nutzten einige Hackermans um (mehr oder weniger freiwillig) ihren Keller zu verlassen, da ihnen der Gang von ihrem Keller zum Gemeinschaftskeller doch etwas fehlte.
+                Dabei kam es immer wieder zu starker Sonnenbestrahlung. Es ist wichtig zu wissen, dass die Sonne einem Hackerman mächtig zu schaffen macht.
+                Einer der Hackermans trank bei einem Ausflug so viel von einem Gesöff namens PluGIN, sodass er sich zu lange in der Sonne aufhielt.
+                Dies führte dazu, dass eine gesamte Änderung von Soft- und Hardeware stattfand und ihn die Hackerman Library verließ.
+                So mussten wir diesen zurücklassen und waren von da an nur noch 10 Hackermans.
+                Nach diesem Schock mussten wir und erst erholen. Doch dafür war keine Zeit.
+                Denn alle guten Dinge sind zwei, wie wir aus dem Binärraum wissen, weshalb es natürlich auch in diesem Jahr eine Runde des Bundeswettbewerbs Informatik
+                mit neuen Aufgaben gab. Doch diesmal war unsere Prozessorentechnik so weit entwickelt, dass doch einige Lösungen für die Aufgaben vorhanden waren.
+                Es reichte jedoch noch lange nicht für einen Preis, aber wir können ja nicht als Pilotprojekt Informatik in allem Vorreiter sein und unsere Nachfolger sollen ja auch noch etwas erreichen XD.
+                Spass bei Seite.
+                Am Ende des Schuljahres sollte eine große Prüfung für die Hackerman-Arbeitgemeinschaft anstehen namens ABITUR. Um dieses überhaupt schreiben zu können,
+                wurden weitere Themen der theoretischen Informatik durch Frau Straeten eingeführt, darunter Dinge wie Graphen, Automaten(gemeint sind nicht Getraenkeautomaten, auch wenn diese für einen Informatiker sehr interesant sein können),
+                was die Köpfe der Hackermans deutlich zum rauchen brachte.
+                Dazu kam, dass die Schutzmaßnahmen, welche die Hackermans vor dem Virus COV19, welcher jede Firewall durchbrechen konnte, schütze, den Kühlvorgang der Hackerman-Prozessoren verhinderte was zur Überhitzung beitrug.
+                Demzufolge war und wurde der Lernprozess immer langsamer. Es ging mit unserer Motivation so bergab wie mit dem Datenschutz von WhatsApp (Threema, hier könnte ihre Werbung stehen).
+                Nachdem sogut wie alle wichtigen Informationen von Frau Straeten in  unsere Fetsplatten eingebrannt waren, ging es nun ans üben und vertiefen. Das fand aber häufig wieder in kleinerern Gemeinschaften statt oder auch alleine.
+                In selten gut besetzten Fragestunden wurde sich mehr über die aktuelle Situation aufgeregt als über Informatik unterhalten.
+                Dann war es soweit, die Prüfung stand vor der Tür.
+                Alle Hackermans waren trotz ihrer gut gepflegten Filesystems doch aufgeregt trotz aller Brechnungen im Vorraus durch.
+                Doch, ohne das Ergebnis zu wissen, hatten wir alle ein ganz gutes Gefühl nach der Abgabe.
+                Aber das sollte es nicht gewesen sein. Die letzte Aufgabe, die die Arbeitgemeinschaft bekam, war sich mit PHP auseinanderzusetzten und eine Webseite zu programmieren. Dass sollten auch die zwei übrigen BK-Hackermans machen.
+                Da aber das Wissen und die Praxis bezüglich dieser Aufgabe fehlte, versuchten sie ihr kleines Unwissen mit einer bombastischen Präsentation zu kompensieren (Bei Interesse gerne an die 0180 6 672255 und bitte Grüße ausrichten).
+                Diese Präsentation und auch die anderen vorgestellten Webseiten waren ein gebührender Abschluss der Hackerman-Arbeitsgemeinschaft.
+                Wir wünschen allen unseren und den noch kommenden Hackermans ein erfolgreiches googlen und kopieren, gnaz nach dem Motto: Copy, Paste and improve
+                Auf ein wiedersehen in irgendwelchen LINUX-Terminals.
+
+                */
+                hackerman.addAbitur(Note == 1111b);
+                hackerman.leaveArbeitsgemeinschaft;
+            }
+        }
+    }
+}
